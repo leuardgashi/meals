ebb8e2530c482091c9def3bb89fe6dc19c7de497
Leuard Gashi <gashileuard@gmail.com>
2025-03-04 02:18:20 +0100

    passing Data to Routes

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -6,9 +6,11 @@ class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
     required this.category,
+    required this.onSelectCategory,
   });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
